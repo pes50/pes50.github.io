5d8ff95cff3ac3c8eb3c021408d8fa805df348da
pes50 <94444440+pes50@users.noreply.github.com>
2022-06-26 22:09:04 +0200

    Updates

---
+++ precache-manifest.a4acb99b1c5409c74b4703adea060b6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f6c103fc9856086c858",
+    "url": "/static/css/main.93a1a223.chunk.css"
+  },
+  {
+    "revision": "6f6c103fc9856086c858",
+    "url": "/static/js/main.6f6c103f.chunk.js"
+  },
+  {
+    "revision": "5a0e4915b1d8800d5d29",
+    "url": "/static/js/1.5a0e4915.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "88f90b4d80926877dc02030c4ba56e9c",
+    "url": "/static/media/background.88f90b4d.jpg"
+  },
+  {
+    "revision": "7cde7eace8fd482b585e33aba4053d7e",
+    "url": "/index.html"
+  }
+];
